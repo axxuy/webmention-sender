2bc8fc81d2b225cd0aee1416105a2779674c37d9
axxuy <axxuyio@gmail.com>
2025-05-18 17:55:48 -0700

    Change feed types to private visibility

--- feed.go
+++ feed.go
@@ -1,14 +1,15 @@
 package feed
 
-type Feed struct {
+type atomFeed struct {
 	Title     string `xml:"title"`
 	Author    string `xml:"author"`
 	NameSpace string `xml:"xmlns,attr"`
 }
-type Entry struct {
-	Link    EntryLink `xml:"link"` //This bit of inderection is needed to get the href. It seems go's xml parser is a bit fiddly with self closing tags
+type atomEntry struct {
+	Link    entryLink `xml:"link"` //This bit of inderection is needed to get the href. It seems go's xml parser is a bit fiddly with self closing tags
 	Content string    `xml:"content"`
+	PubDate string    `xml:"published"`
 }
-type EntryLink struct {
+type entryLink struct {
 	Link string `xml:"href,attr"`
 }
