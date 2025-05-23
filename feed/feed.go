306555fd44126a31aca84796361f3542cff41a6a
axxuy <axxuyio@gmail.com>
2025-05-22 19:15:35 -0700

    Move feed.go to it's own directory

--- feed/feed.go
+++ feed/feed.go
@@ -3,6 +3,10 @@ package feed
 import (
 	"encoding/xml"
 	"errors"
+	"strings"
+
+	"github.com/axxuy/webmention-sender/util"
+	"golang.org/x/net/html"
 )
 
 type atomFeed struct {
@@ -13,6 +17,7 @@ type atomFeed struct {
 type atomEntry struct {
 	Link    entryLink `xml:"link"` //This bit of inderection is needed to get the href. It seems go's xml parser is a bit fiddly with self closing tags
 	Content string    `xml:"content"`
+	Id      string    `xml:"id"`
 	PubDate string    `xml:"published"`
 }
 type entryLink struct {
@@ -23,6 +28,30 @@ type Entry struct {
 	Id    string
 	Url   string
 	Links []string
+}
+
+func convertEntry(entry atomEntry) (Entry, error) {
+	result := Entry{}
+	result.Id = entry.Id
+	result.Url = entry.Link.Link
+	contentHtml, err := html.Parse(strings.NewReader(entry.Content))
+	if err != nil {
+		return Entry{}, err
+	}
+	linkNodes, err := util.FindAllByTag(*contentHtml, "a")
+	if err != nil {
+		return Entry{}, err
+	}
+	links := make([]string, 0)
+	for _, node := range linkNodes {
+		for _, attr := range node.Attr {
+			if attr.Key == "href" {
+				links = append(links, attr.Val)
+			}
+		}
+	}
+	result.Links = links
+	return result, nil
 }
 
 func ParseAtomFeed(data []byte) ([]Entry, error) {
