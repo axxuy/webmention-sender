a430485b5d76303b9e1c4108494a3f8a73ff11fc
axxuy <axxuyio@gmail.com>
2025-05-25 14:08:36 -0700

    Add utility function for getting a delimited substring

--- util/util.go
+++ util/util.go
@@ -2,10 +2,25 @@ package util
 
 import (
 	"errors"
+	"strings"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
+
+// return a substring of s between before and after. If either separator does not appear, return ""
+func CutSubString(before, after, s string) string {
+	_, suffix, found := strings.Cut(s, before)
+	if !found {
+		return ""
+	}
+	substr, _, found := strings.Cut(suffix, after)
+	if !found {
+		return ""
+	}
+	return substr
+
+}
 
 func FindAllByTag(doc html.Node, tag string) ([]*html.Node, error) {
 	tagAtom := atom.Lookup([]byte(tag))
