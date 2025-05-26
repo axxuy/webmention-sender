b825e9e3dd84671d16f14d578f63f1c91343113c
axxuy <axxuyio@gmail.com>
2025-05-26 10:17:39 -0700

    Add more testcases for webmention

--- webmention/webmention_test.go
+++ webmention/webmention_test.go
@@ -3,8 +3,51 @@ package webmention
 import "testing"
 
 func Test_parseLinkHeader(t *testing.T) {
+	//Empty header
+	testEndpoint := "http://example.com/webmention"
 	headers := []string{""}
-	if parseLinkHeader(headers) != "" {
+	result := parseLinkHeader(headers)
+	if result != "" {
 		t.Error("Empty header should have empty result")
 	}
+	//Link header but no endpoint
+	headers = []string{`<http://example.com/style.css>; rel=stylesheet`}
+	result = parseLinkHeader(headers)
+	if result != "" {
+		t.Errorf("Expected %q, got %q\n", "", result)
+	}
+
+	//Webmention link but no rel
+	headers = []string{`<http://example.com/webmention>; rel=stylesheet`}
+	result = parseLinkHeader(headers)
+	if result != "" {
+		t.Errorf("Expected %q, got %q\n", "", result)
+	}
+
+	//Simple webmention link
+	headers = []string{`<http://example.com/webmention>; rel=webmention`}
+	result = parseLinkHeader(headers)
+	if result != testEndpoint {
+		t.Errorf("Expected %q, got %q\n", testEndpoint, result)
+	}
+	//Simple webmention link with quoted rel
+	headers = []string{`<http://example.com/webmention>; rel="webmention"`}
+	result = parseLinkHeader(headers)
+	if result != testEndpoint {
+		t.Errorf("Expected %q, got %q\n", testEndpoint, result)
+	}
+	// webmention link with query param
+	headers = []string{`<http://example.com/webmention/?rel=webmention>`}
+	result = parseLinkHeader(headers)
+	if result != "" {
+		t.Errorf("Expected %q, got %q\n", testEndpoint, result)
+	}
+
+	//Multiple links in header
+	headers = []string{`<http://example.com/foo>, <http://example.com/webmention>; rel=webmention`}
+	result = parseLinkHeader(headers)
+	if result != testEndpoint {
+		t.Errorf("Expected %q, got %q\n", testEndpoint, result)
+	}
+
 }
