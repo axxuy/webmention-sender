0d794c08752d0fdf6bc0b351898135bd138e9ba5
axxuy <axxuyio@gmail.com>
2025-05-25 19:39:22 -0700

    Check http Link headers for webmention endpoints

--- webmention/webmention.go
+++ webmention/webmention.go
@@ -3,6 +3,10 @@ package webmention
 import (
 	"net/http"
 	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/axxuy/webmention-sender/util"
 )
 
 type Endpoint struct {
@@ -10,6 +14,29 @@ type Endpoint struct {
 	url    *url.URL
 }
 
+// Check the http Link header for a url with rel="webmention"
+// https://httpwg.org/specs/rfc8288.html#header
+// Returns "" if none present
+func parseLinkHeader(header []string) string {
+	for _, headerVal := range header {
+		if !strings.Contains(headerVal, "webmention") {
+			continue
+		}
+		headerFields := strings.Split(headerVal, ",")
+		for _, field := range headerFields {
+			fieldParts := strings.Split(field, ";")
+			for _, part := range fieldParts {
+				attrPattern := regexp.MustCompile(`rel\s*=\s*"?\s*webmention`)
+				if attrPattern.MatchString(part) {
+					link := util.CutSubString("<", ">", part)
+					return link
+				}
+			}
+		}
+
+	}
+	return ""
+}
 func GetWebmentionEndpoint(targetUrl *url.URL) (*Endpoint, error) {
 }
 
