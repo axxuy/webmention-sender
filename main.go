113c467fb59ad770a04698753cce5a9294ddddf3
axxuy <axxuyio@gmail.com>
2025-06-11 20:44:03 -0700

    Add flag to indicate first run on a feed and override the interval setting

--- main.go
+++ main.go
@@ -14,14 +14,19 @@ func main() {
 	var interval int
 	flag.StringVar(&feedUrl, "feed", "", "Url of the rss feed to monitor")
 	flag.IntVar(&interval, "interval", 6, "Time in hours since the feed was last checked")
+	firstRun := flag.Bool("first-run", false, "Is this the first time you have checked this feed?")
 	flag.Parse()
 	if feedUrl == "" {
 		log.Fatal("No feed given")
 	}
 	var lastRun time.Time
-	delta := -time.Hour * time.Duration(interval)
-	now := time.Now()
-	lastRun = now.Add(delta)
+	if *firstRun {
+		lastRun = time.Time{}
+	} else {
+		delta := -time.Hour * time.Duration(interval)
+		now := time.Now()
+		lastRun = now.Add(delta)
+	}
 	entries, err := feed.Fetch(feedUrl, &lastRun)
 	if err != nil {
 		log.Fatal("Could not retrieve feed: " + err.Error())
